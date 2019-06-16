e44bb7d0ed712c0da3c7f2eba05052036908ba9d
Aaron Macken <aaron.a.macken@gmail.com>
2019-06-16 15:47:39 -0700

    Created a partials file for the nav bar and included a navbar variable in all view files in order to reduce WET code. Created a route for the register form and updated the href for the navbar. Created register EJS and CSS files. The form could still use a little polishing though.

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,28 @@
-// Index Routes File
-
 // Require Vars
 const express = require("express"),
     router = express.Router();
+
+
+// ------------ Basic Routes ------------ //
 
 // Landing Route
 router.get("/", (req, res) => {
     res.render("indexViews/landing");
 });
 
+// Juicer route
 router.get("/browse", (req, res) => {
     res.render("indexViews/browse");
 });
+
+
+// ------------ Register Routes ------------ //
+
+// Show register form
+router.get("/register", (req, res) => {
+    res.render("authViews/register");
+});
+
+// Register POST route
 
 module.exports = router;
