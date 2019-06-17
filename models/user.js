066d47a864daf169a3503daa616b6e7cd2bf6f32
Aaron Macken <aaron.a.macken@gmail.com>
2019-06-16 19:29:37 -0700

    Created models directory and added user.js file to it. Added User Schema inside of it.

---
+++ models/user.js
@@ -0,0 +1,16 @@
+// Require Package Vars
+const mongoose = require("mongoose"),
+    passportLocalMongoose = require("passport-local-mongoose");
+
+// Mongoose User Schema
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+// Include passport-local-mongoose package functionality into the User Schema
+userSchema.plugin(passportLocalMongoose);
+
+// Export User Model
+module.exports = mongoose.model("User", userSchema);
+
