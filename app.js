2fa12e260b7365a03b731e09c0b9cfe3e128124f
Aaron Macken <aaron.a.macken@gmail.com>
2019-06-16 19:22:20 -0700

    Preparing the back end code for the user register page. Added necessary packages and code for using these packages in the app JS file.

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
-// Require Vars //
+// Require Package Vars //
 const express = require("express"),
     app = express(),
-    bodyParser = require("body-parser");
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    passport = require("passport"), 
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose"),
+    flash = require("connect-flash");
 
 
+// Require Model Vars //
+const User = require("./models/user");
 
 // App Configure //
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(flash());
+app.use(require("express-session")({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Connect MongoDB //
+mongoose.connect(process.env.DATABASEURL || "mongodb://localhost:27017/INKSite", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => {
+    console.log("Connected to DB!");
+}).catch(err => {
+    console.log("Error", err.message);
+})
+
 
 // Require Routes //
 const indexRoutes = require("./routes/index");
