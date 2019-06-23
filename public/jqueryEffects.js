153cb99a8cc5b1e0c198ad87450370b149c6fd0b
Aaron Macken <aaron.a.macken@gmail.com>
2019-06-23 12:08:48 -0700

    Added a scroll effect for one of the buttons on the landing page. Renamed stickyNav to jqueryEffects. Added a link to the browse page on the landing pages first ghost button.

--- public/jqueryEffects.js
+++ public/jqueryEffects.js
@@ -1,19 +1,21 @@
-
-// Menu toggle-button
-
+// Nav menu toggle-button
 $("document").ready(function() {
     $(".menu-icon").on("click", function() {
         $("nav ul").toggleClass("showing");
     });
 });
 
-
-//  
-
+// Nav scroll effect
 $(window).on("scroll", function() {
     if($(window).scrollTop()){
         $("nav").addClass("black");
     } else {
         $("nav").removeClass("black");
     }
-})
+});
+
+// Scroll on button click effect, requires waypoints CDN
+$(".js--scroll-conventions").click(function() {
+    // the-50 is to offset the fixed nav
+    $("html, body").animate({scrollTop: $(".js--section-signUp").offset().top-50}, 1000);
+});
