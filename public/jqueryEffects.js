4c76a5aa937d25ba76e8b0a5411f22a7fe2febeb
Aaron Macken <aaron.a.macken@gmail.com>
2019-06-23 12:38:24 -0700

    Added some animation effects using the animate.CSS framework, waypoints, jquery and CSS.

--- public/jqueryEffects.js
+++ public/jqueryEffects.js
@@ -19,3 +19,22 @@ $(".js--scroll-conventions").click(function() {
     // the-50 is to offset the fixed nav
     $("html, body").animate({scrollTop: $(".js--section-signUp").offset().top-50}, 1000);
 });
+
+// Animation on scroll with animate.css and waypoints
+$(".js--wp-1").waypoint(function(direction) {
+    $(".js--wp-1").addClass("animated fadeIn");
+}, {
+    offset: "80%"
+});
+
+$(".js--wp-2").waypoint(function(direction) {
+    $(".js--wp-2").addClass("animated fadeInUp");
+}, {
+    offset: "60%"
+});
+
+$(".js--wp-3").waypoint(function(direction) {
+    $(".js--wp-3").addClass("animated fadeIn");
+}, {
+    offset: "70%"
+});
