fbfba8170770b5475e0f6068e195cf4d630f3e64
Aaron Macken <aaron.a.macken@gmail.com>
2019-06-24 18:10:34 -0700

    Add icons next to tab content and about card for later CRUD functionality. Create a modal window that appears in center of screen when about button is clicked.

--- public/tab.js
+++ public/tab.js
@@ -1,4 +1,5 @@
-function openTab(evt, cityName) {
+// =========================================  TAB =========================================  //
+function openTab(evt, tabName) {
     // Declare all variables
     var i, tabcontent, tablinks;
   
@@ -15,10 +16,37 @@ function openTab(evt, cityName) {
     }
   
     // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
   }
   
-  // Get the element with id="defaultOpen" and click on it
+  // Get the element with id="defaultOpen" and select it
   document.getElementById("defaultOpen").click();
   
+
+// =========================================  MODAL =========================================  //
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
