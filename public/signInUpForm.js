4d2174abcc49effae66d3191876be90098ba697b
Aaron Macken <aaron.a.macken@gmail.com>
2019-07-05 11:45:37 -0700

    Create media queries for login page. Fix bug with repeating background image. Create a reusable javascript file that will be triggered when alert messages are shown on the login page or the register page.

---
+++ public/signInUpForm.js
@@ -0,0 +1,8 @@
+// ========================================= Style  =========================================  //
+
+var alert = document.querySelector(".alert");
+var myForm = document.querySelector("form");
+if(alert) {
+  myForm.style.marginTop = "-55px";
+  console.log("Yeet");
+}
